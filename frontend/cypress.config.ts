af465058d9e3e0ebc91854be78c07a2e59d5f29f
Vicky Koblinski <vickykoblinski@gmail.com>
2023-03-02 16:10:41 -0500

    8084 -> 8085

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:8084',
+    baseUrl: 'http://localhost:8085',
   },
 });
