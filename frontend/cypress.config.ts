faac30f4bb224c058d1b0bb8705f8fbab7b09c85
Vicky Koblinski <vickykoblinski@gmail.com>
2023-03-02 21:40:15 -0500

    localhost -> [::1]

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3002',
+    baseUrl: 'http://[::1]:3002',
   },
 });
